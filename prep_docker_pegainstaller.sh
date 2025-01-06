567675a529aef93dc245d0645ea9fb5b05963a5b
Andrew Etuk <awetuk@gmail.com>
2025-01-06 16:42:30 +0000

    Update prep_docker_pegainstaller.sh

--- prep_docker_pegainstaller.sh
+++ prep_docker_pegainstaller.sh
@@ -1,8 +1,8 @@
 sh pegadockerlogin
-BUILD_TAG="abc123"
+BUILD_TAG="8.8.0-ga"
 REGISTRY_HOSTNAME="abcd"
 REGISTRY_PORT=1234
 VERSION=1.0.1
 docker pull pega-docker.downloads.pega.com/platform/pega:$VERSION
-docker tag pega-docker.downloads.pega.com/platform/pega:$VERSION $REGISTRY_HOSTNAME:$REGISTRY_PORT/pega:8.8.0-ga
-docker push $REGISTRY_HOSTNAME:$REGISTRY_PORT/pega:8.8.0-ga
+docker tag pega-docker.downloads.pega.com/platform/pega:$VERSION $REGISTRY_HOSTNAME:$REGISTRY_PORT/pega:$BUILD_TAG
+docker push $REGISTRY_HOSTNAME:$REGISTRY_PORT/pega:$BUILD_TAG
