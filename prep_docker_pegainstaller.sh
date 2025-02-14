d00986d596353ac2a76f887da9780a4c9cd84fcc
Andrew Etuk <awetuk@gmail.com>
2025-02-14 18:22:58 +0000

    Update prep_docker_pegainstaller.sh

--- prep_docker_pegainstaller.sh
+++ prep_docker_pegainstaller.sh
@@ -1,6 +1,6 @@
 sh pegadockerlogin.sh
 REGISTRY_NAME="kuzdune2/installer"
-docker pull pega-docker.downloads.pega.com/platform/installer:
+docker pull pega-docker.downloads.pega.com/platform/installer
 sh pegauserdockerlogin.sh
 docker tag pega-docker.downloads.pega.com/platform/installer $REGISTRY_NAME
 docker push $REGISTRY_NAME
