b75806ed7a737065cf3c7299d62d7658ef6456c7
Andrew Etuk <awetuk@gmail.com>
2025-02-17 20:48:29 +0000

    a

--- pega24/upgrade.sh
+++ pega24/upgrade.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Run as :
+# tmux new-session -d -s upgrade 'sh upgrade.sh'
+#  or
+# Type Ctrl+p then Ctrl+q. It will help you to turn interactive mode to daemon mode.
+
 DOCKER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
 
 #Postgres container db
