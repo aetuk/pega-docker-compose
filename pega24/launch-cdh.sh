5d57a37a90a116b764de97f7595d3cbfc065b68a
Andrew Etuk <awetuk@gmail.com>
2025-02-18 12:00:46 +0000

    Update launch-cdh.sh

--- pega24/launch-cdh.sh
+++ pega24/launch-cdh.sh
@@ -5,6 +5,9 @@
 # get hostname, tier and start
 cd "$(dirname "$0")"
 
+PEGA_HOST=$(cat /etc/hostname)
+export PEGA_HOST
+
 export PEGA_TIER=cdh
 export PEGA_TYPE=ADM,Batch,RealTime,RTDG,Search,BackgroundProcessing
 export CASSANDRA_CLUSTER=true
