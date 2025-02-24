e51163fff7759d1facdc86ee669bb28adc528ca5
Andrew Etuk <awetuk@gmail.com>
2025-02-24 10:53:24 +0000

    Create launch-local.sh

---
+++ pega24/launch-local.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DOCKER_LHOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+export DOCKER_LHOST
+
+#TODO Start jdbc local server
+
+# get hostname, tier and start
+cd "$(dirname "$0")"
+
+PEGA_HOST=$(cat /etc/hostname)
+export PEGA_HOST
+
+export PEGA_TIER=cdh
+export PEGA_TYPE=ADM,Batch,RealTime,RTDG,Search,BackgroundProcessing
+export CASSANDRA_CLUSTER=true
+export CASSANDRA_NODES=cassandra
+docker compose --profile cdh up -d
