ef58fdb9efcec0f324e49ba18c31466ad4e03d63
Andrew Etuk <awetuk@gmail.com>
2025-02-27 14:01:07 +0000

    updated

--- prep_docker_search-n-reporting-service.sh
+++ prep_docker_search-n-reporting-service.sh
@@ -1,6 +1,6 @@
 sh pegadockerlogin.sh
 REGISTRY_NAME="kuzdune4/search-n-reporting-service"
-docker pull pega-docker.downloads.pega.com/platform-services/search-n-reporting-service
+docker pull pega-docker.downloads.pega.com/platform-services/search-n-reporting-service:1.37.3
 sh pegaconsuserdockerlogin.sh
-docker tag pega-docker.downloads.pega.com/platform-services/search-n-reporting-service $REGISTRY_NAME
+docker tag pega-docker.downloads.pega.com/platform-services/search-n-reporting-service:1.37.3 $REGISTRY_NAME
 docker push $REGISTRY_NAME
